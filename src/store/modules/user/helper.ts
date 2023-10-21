045de7a1a2640807f625684592e34bc61fed4ee8
shiguang <201430064@qq.com>
2023-10-21 20:15:50 +0800

    修改配置

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://img.tucang.cc/api/image/show/07a4bcfca77d45e9aefd19e82637991f',
+      name: 'Iron Man',
+      description: "It's an imperfect world, but it's the only one we've got.",
     },
   }
 }
